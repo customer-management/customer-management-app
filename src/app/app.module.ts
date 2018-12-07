61de69ce34b328aa44b20d97b35c141812b5eaea
US\arnamondal <arnamondal@deloitte.com>
2018-12-07 17:50:27 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { CenterPanelComponent } from './center-panel/center-panel.component';
+import {AddCustomerComponent} from './center-panel/add-customer/add-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavigationComponent,
+    RightPanelComponent,
+    CenterPanelComponent,
+    AddCustomerComponent
   ],
   imports: [
     BrowserModule
